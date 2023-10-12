2fa60bda9d64739856c7e0420c2c99f622c64e92
Sean Kelly Dausen <146722016+skdausen@users.noreply.github.com>
2023-10-12 07:43:02 -0700

    feat: Activity 8 in DSA
    Arrays in JavaScript

---
+++ script.js
@@ -0,0 +1,24 @@
+// Activity 8
+
+/*
+1. Create array name (favSingers) Store 3 favorite singers.
+2. console.log the all the singers in that array.
+3. Create array name (favNumbers) & store 4 fav Numbers.
+    console.log all in favNumbers array.
+4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
+5. Now Access each item in that array by using [] notation.
+    favSingers[0],  favSingers[1],  favSingers[2],  favNumbers [1] and lastly mixedArr[3]
+*/
+
+const favSingers = ["Twice", "Anson Seabra", "YoungK"];
+const favNumbers = [8, 10, 19, 96];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers);
+console.log(favNumbers);
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
